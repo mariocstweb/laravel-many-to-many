06949b41331dbb82bbeef48bc94f215e023a9a1b
mariocstweb <mariocstmra@gmail.com>
2024-03-26 15:11:58 +0100

    created technology seeder

--- database/migrations/2024_03_26_134402_create_technologies_table.php
+++ database/migrations/2024_03_26_134402_create_technologies_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('label', 15)->unique();
+            $table->string('color', 15)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $techs = [
+            ['label' => 'HTML', 'color' => 'danger'],
+            ['label' => 'CSS', 'color' => 'primary'],
+            ['label' => 'ES6', 'color' => 'waring'],
+            ['label' => 'Bootstrap', 'color' => 'dark'],
+            ['label' => 'Vue', 'color' => 'success'],
+            ['label' => 'SQL', 'color' => 'secondary'],
+            ['label' => 'PHP', 'color' => 'info'],
+            ['label' => 'Laravel', 'color' => 'danger']
+        ];
+
+        foreach ($techs as $tech) {
+            $new_tech = new Technology();
+
+            $new_tech->label = $tech['label'];
+            $new_tech->color = $tech['color'];
+
+            $new_tech->save();
+        }
     }
 }
